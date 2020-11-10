ad74919517b2915e9b9c218025b4ddc9f2878376
wskmcserver1 <72479817+wskmcserver1@users.noreply.github.com>
2020-11-10 16:59:12 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -28,7 +28,7 @@ tmate -S /tmp/tmate.sock wait tmate-ready
 echo ________________________________________________________________________________
 echo
 echo To connect to this session copy-n-paste the following into a terminal:
-tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}'
+# tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}'
 echo After connecting you can run 'touch /tmp/keepalive' to disable the 15m timeout
 
 if [[ ! -z "$SLACK_WEBHOOK_URL" ]]; then
